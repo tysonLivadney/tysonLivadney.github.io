b979c7a48868664c84c89d4fd3b11d81dbb559fa
tyson779 <tysonlivadney04@gmail.com>
2024-05-12 19:34:33 -0700

    Final

--- script.js
+++ script.js
@@ -4,12 +4,23 @@
     fullscreenBtns.forEach(btn => {
         btn.addEventListener('click', function () {
             const container = this.parentElement;
-            if (container.requestFullscreen) {
-                container.requestFullscreen();
-            } else if (container.webkitRequestFullscreen) { /* Safari */
-                container.webkitRequestFullscreen();
-            } else if (container.msRequestFullscreen) { /* IE/Edge */
-                container.msRequestFullscreen();
+
+            if (!document.fullscreenElement) {
+                if (container.requestFullscreen) {
+                    container.requestFullscreen();
+                } else if (container.webkitRequestFullscreen) { /* Safari */
+                    container.webkitRequestFullscreen();
+                } else if (container.msRequestFullscreen) { /* IE/Edge */
+                    container.msRequestFullscreen();
+                }
+            } else {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (document.webkitExitFullscreen) { /* Safari */
+                    document.webkitExitFullscreen();
+                } else if (document.msExitFullscreen) { /* IE/Edge */
+                    document.msExitFullscreen();
+                }
             }
         });
     });
